7e4619ac4e3b87a114cfa0eaa99748a955dd604a
JorgenDIF <jorgenjonsson80@gmail.com>
2025-01-19 18:51:30 +0100

    chore : add models to admin.py

--- resource_item/admin.py
+++ resource_item/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Category, ResourceItem
 # Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(ResourceItem)
--- resource_item/models.py
+++ resource_item/models.py
@@ -18,9 +18,9 @@ class ResourceItem(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE,
-                                    related_name="resource item")
+                                    related_name="resource_item")
     user_id = models.ForeignKey(User, on_delete=models.CASCADE,
-                                related_name="created by")
+                                related_name="created_by")
     url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

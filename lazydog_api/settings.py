1a4fd4e42c1ae824eee0ceaace0c240ede7b09e2
JorgenDIF <jorgenjonsson80@gmail.com>
2025-03-27 10:29:18 +0100

    fix(settings): remove duplicate 'comment' app from INSTALLED_APPS

--- lazydog_api/settings.py
+++ lazydog_api/settings.py
@@ -37,7 +37,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
-    
+
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ]
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
     'category',
     'flag',
     'tag',
-    'comment'
+    
 ]
 
 MIDDLEWARE = [
